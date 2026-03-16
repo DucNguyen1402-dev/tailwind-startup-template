9b561d197a403242b0748e2702ba79278fe0fe0b
Duc Nguyen <hoangduc140220@gmail.com>
2026-03-16 11:43:15 +0700

    feat: implement responsive testimonials feature with dark mode and animations

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,35 @@ AOS.init({
   disable: "mobile",
 });
 
+// Glightbox
 const lightbox = GLightbox({
   selector: ".glightbox",
   touchNavigation: true,
   loop: true,
   autoplayVideos: true,
   width: "900px",
+});
+
+$(document).ready(function () {
+  $(".achievement__value").countUp();
+  $(".testimonial-cards")
+    .on("init", function () {
+      AOS.refresh();
+    })
+    .slick({
+      speed: 1000,
+      autoplay: true,
+      arrows: false,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplaySpeed: 5000,
+    });
+
+  addBackToTop({
+    diameter: 50,
+    backgroundColor: "#6366F1",
+    textColor: "#fff",
+  });
 });
